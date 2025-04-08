8406fad460d4550f3a79d0144a549c8cbd3b5c7e
Eveliina Hampus <112644513+eveliinahampus@users.noreply.github.com>
2025-04-08 23:21:59 +0300

    md-to-pdf / Puppeteer based approach (#2)
    * test
    
    * feature: HTML template
    
    * chore: stylesheet
    
    * chore: add DataTribe logo
    
    * chore: Updated stylesheet path
    
    * chore: stylesheet - code block debug
    
    * YML update
    - Debian slim image for faster processing
    - Finds markdown files under subfolders
    - Targets better to find correct CSS file
    
    Problems
    - Inherits font weight and casing 'incorrectly', requires inspection
      from the HTML file. Find correct HTML tag and address that in CSS.
    
    Not checked
    - How behaves with multiple pages
    
    * h1 behavior check
    
    * Debugged span styling for code blocks
    
    * Version 2: md-to-pdf approach
    
    Detailed description:
    md-to-pdf, a Puppeteer based approach
    - WHY? Pandoc and  wkhtmlpdf didn't offer enough support for custom CSS.
    Nuances in this approach?
    - Puppeteer can be run on Ubuntu 22.04, but due security and sandboxing,
    not in newer versions.
    - SVG image encoded base64. Only format that seemed to print on PDF with
      this script.
    
    NOTICE: Prior any changes, a troughout review of sourcecode is
    necessary. Many behaviors were not documented.
    
    References: https://github.com/simonhaenisch/md-to-pdf

---
+++ config.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const logoBase64 = fs.readFileSync('./logo.svg', { encoding: 'base64' });
+const logoDataUrl = `data:image/svg+xml;base64,${logoBase64}`;
+
+
+module.exports = {
+    stylesheet: ['./layout.css'],
+    marked_options: {
+        headerIds: true,
+        smartypants: true,
+    },
+    pdf_options: {
+        format: 'A4',
+        margin: {
+            top: '130px',
+            bottom: '100px',
+            left: '32px',
+            right: '32px',
+        },
+        printBackground: true,
+        displayHeaderFooter: true,
+        headerTemplate: `
+        <style>
+        section {
+            width: 100%;
+            text-align: center;
+            align-items: center;
+            padding: 0 7px;
+            background: #1b7182 !important;
+            -webkit-print-color-adjust: exact;
+            text-transform: uppercase;
+            font-family: 'League Spartan', sans-serif;
+            display: block;
+            justify-content: center;
+        }
+        .logo img {
+            max-width: 70px;
+        }
+        </style>
+            
+        <section>>
+        <div style="height: 70px; margin-bottom: 3px;">
+            <div class="logo">
+                <img src="${logoDataUrl}" alt="Logo" style="width: 60px; height: 60px;" />
+            </div>
+            <div>
+                <span style='color: white; font-weight: bold; font-size: 10pt'>Data</span>
+                <span style='color: #fb5a01; font-weight: bold; font-size: 10pt'>Tribe</span>
+            </div>
+            </div>
+        </section>
+        `,
+        footerTemplate: `
+        <style>
+        section {
+            width: 100%;
+            font-weight: normal;
+            padding: 10px;
+            background: #1b7182 !important;
+            -webkit-print-color-adjust: exact;
+            font-family: 'League Spartan', sans-serif;
+            display: block;
+        }
+        </style>
+        <section>
+            <span style='font-size: 9pt; text-transform: none; height: 40px; text-align: center; align-items: center;'>
+                PDF generated on: ${new Date().toLocaleDateString()}</span>
+        </section>
+        `,
+    },
+    stylesheet_encoding: 'utf-8',
+};
